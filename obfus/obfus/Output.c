512ff73b882bf9611323ff56f682e9492dd1e81e
Arkagami <mixno20@gmail.com>
2017-03-13 14:03:58 +0300

    Стабильно работает. 3 раза подряд себя нормально обфусцирует.

--- obfus/obfus/Output.c
+++ obfus/obfus/Output.c
@@ -2,288 +2,285 @@
 #include <stdio.h> 
 #include <string.h> 
 
+int fun93 = 0, fun04 = 0; 
+char fun14[10000], fun24 = 'n', fun34 = 'n', fun44[10000], fun54 = 'n', fun64 = 'n';
+int fun74 = strlen(fun14);
+int fun841 = 0, fun942 = 0; 
 
-int fun8383 = 0, fun9393 = 0; 
-char fun0404[10000], fun8314 = 'n', fun8324 = 'n', fun8334[10000], fun8344 = 'n', fun8354 = 'n';
-int fun8364 = strlen(fun8304);
-int fun83741 = 0, fun83842 = 0; 
+void fun05(char fun15[], char fun25[]) {
+	FILE *fun35 = fopen(fun15, "r");
+	FILE *fun45 = fopen(fun25, "w");
 
-void fun8394(char fun8305[], char fun8315[]) {
-	FILE *fun8325 = fopen(fun8305, "r");
-	FILE *fun8335 = fopen(fun8315, "w");
+	for (fun93 = 0; fun93<10000; fun93++) { fun44[fun93] = fun14[fun93] = '\0'; }
 
-	for (fun8383 = 0; fun8383<10000; fun8383++) { fun8334[fun8383] = fun8304[fun8383] = '\0'; }
-
-	while (!feof(fun8345)) {
-	fun8355:
-		fun8314 = fgetc(fun8325);
-		if (feof(fun8325)) { break; }
-	fun8365:
+	while (!feof(fun35)) {
+	fun55:
+		fun24 = fgetc(fun35);
+		if (feof(fun35)) { break; }
+	fun65:
 		
-		if (fun8314 == '\"') {
-			fputc('\"', fun8335);
-			fun8383 = 0;
-			fun8393 = 0;
-			fun8324 = 'n';
-			fun8314 = '#';
-			while ((fun8314 != '\"') || (fun8393 % 2 != 0)) {
-			fun8375:fun8324 = fun8314;
-				fun8314 = fgetc(fun8325);
-				if (feof(fun8325)) { goto fun8385; }
-				if (((fun8314 == '\n') && (fun8324 == '\\') && (fun8393 % 2 == 1))) { fputc('\n', fun8335); fun8393 = 0; goto fun8395; }
-				else { if (fun8314 == '\n') { fputc('\n', fun8335); goto fun8355; } }
-				if (fun8314 == '\\') {
-					if (fun8324 == '\"') { fun8393 = 1; }
-					else { fun8393++; }
+		if (fun24 == '\"') {
+			fputc('\"', fun45);
+			fun93 = 0;
+			fun04 = 0;
+			fun34 = 'n';
+			fun24 = '#';
+			while ((fun24 != '\"') || (fun04 % 2 != 0)) {
+			fun75:fun34 = fun24;
+				fun24 = fgetc(fun35);
+				if (feof(fun35)) { goto fun85; }
+				if (((fun24 == '\n') && (fun34 == '\\') && (fun04 % 2 == 1))) { fputc('\n', fun45); fun04 = 0; goto fun75; }
+				else { if (fun24 == '\n') { fputc('\n', fun45); goto fun55; } }
+				if (fun24 == '\\') {
+					if (fun34 == '\"') { fun04 = 1; }
+					else { fun04++; }
 				}
-				else { if (fun8314 != '\"') { fun8393 = 0; } }
-				fputc(fun8314, fun8335);
+				else { if (fun24 != '\"') { fun04 = 0; } }
+				fputc(fun24, fun45);
 			}
 			
-			goto fun8355;
+			goto fun55;
 		}
 		else {
 			
 			
-			if (fun8314 == '\'') {
+			if (fun24 == '\'') {
 				fputc('\'', fout);
-				fun8383 = 0;
-				fun8393 = 0;
-				fun8324 = 'n';
-				fun8314 = '#';
-				while ((fun8314 != '\'') || (slkol % 2 != 0)) {
-				fun8324 = fun8314;
-					fun8314 = fgetc(fun8325);
-					if (feof(fun8325)) { goto fun8306; }
-					if (fun8314 == '\n') { fputc('\n', fun8335); goto fun8355; }
-					if (fun8314 == '\\') {
-						if (fun8324 == '\'') { slkol = 1; }
-						else { fun8393++; }
+				fun93 = 0;
+				fun04 = 0;
+				fun34 = 'n';
+				fun24 = '#';
+				while ((fun24 != '\'') || (slkol % 2 != 0)) {
+					fun34 = fun24;
+					fun24 = fgetc(fun35);
+					if (feof(fun35)) { goto fun95; }
+					if (fun24 == '\n') { fputc('\n', fun45); goto fun55; }
+					if (fun24 == '\\') {
+						if (fun34 == '\'') { slkol = 1; }
+						else { fun04++; }
 					}
-					else { if (fun8314 != '\'') { slkol = 0; } }
-					fputc(fun8314, fun8335);
+					else { if (fun24 != '\'') { slkol = 0; } }
+					fputc(fun24, fun45);
 				}
-				goto fun8355;
+				goto fun55;
 			}
 			else {
 				
 
 				
-				if (fun8314 == '/') {
-					fun8314 = fgetc(fun8325); if (feof(fun8325)) { fputc('/', fun8335); goto fun8385; }
-					if (fun8314 == '/') {
-						fputc('\n', fun8335); while (1) {
-							fun8324 = fun8314;
-							fun8314 = fgetc(fun8325);
-							if (feof(fun8325)) { goto fun8385; }
-							if (fun8314 == '\n') {
-								if (fun8324 == '\\') {
+				if (fun24 == '/') {
+					fun24 = fgetc(fun35); if (feof(fun35)) { fputc('/', fun45); goto fun85; }
+					if (fun24 == '/') {
+						fputc('\n', fun45); while (1) {
+							fun34 = fun24;
+							fun24 = fgetc(fun35);
+							if (feof(fun35)) { goto fun85; }
+							if (fun24 == '\n') {
+								if (fun34 == '\\') {
 									continue;
 								}
 								else { break; }
 							}
 						}
-						goto fun8355;
+						goto fun55;
 					}
 					else {
-						if (fun8314 == '*') {
-							fun8344 = 'i'; fun8354 = 'i'; while ((fun8344 != '*') || (fun8354 != '/')) { fun8344 = fun8354; fun8354 = fgetc(fun8325); if (feof(fun8325)) { goto fun8385; } } goto fun8355;
+						if (fun24 == '*') {
+							fun54 = 'i'; fun64 = 'i'; while ((fun54 != '*') || (fun64 != '/')) { fun54 = fun64; fun64 = fgetc(fun35); if (feof(fun35)) { goto fun85; } } goto fun55;
 						}
 						else {
-							fputc('/', fun8335); goto fun8365;
+							fputc('/', fun45); goto fun65;
 						}
 					}
-					goto fun8355;
+					goto fun55;
 				}
-				else { fputc(fun8314, fun8335); }
+				else { fputc(fun24, fun45); }
 			}
 		}
 
 		
 
 	}
-fun8385:
-	fclose(fun8325);
-	fclose(fun8335);
+fun85:
+	fclose(fun35);
+	fclose(fun45);
 }
 
 
-int fun8316 = 0, fun8326 = 0, fun8336 = 0;
-char fun8346[53] = "qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM", fun8356[10000][100], fun8366[10000][100];
+int fun06 = 0, fun16 = 0, fun26 = 0;
+char fun36[53] = "qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM", fun46[10000][100], fun56[10000][100];
 
-int fun8376(char fun8386) {
-	for (int fun8396 = 0; fun8307 < 53; fun8317++) if (fun8386 == fun8346[fun8396]) return 1;
+int fun66(char fun76) {
+	for (int fun86 = 0; fun96 < 53; fun86++) if (fun76 == fun36[fun86]) return 1;
 	return 0;
 }
 
 
-char *fun8327() {
-	int fun8337 = 0, fun8347=0;
-	for (int fun8357 = 0; fun8367 < fun8326-2; fun8377++) {
-		fun8337 = 0;
-		while (fun8356[fun8357][fun8337] || fun8356[fun8326][fun8337]) {
-			fun8347 = fun8357; if (fun8356[fun8357][fun8337] != fun8356[fun8326][fun8337]) goto fun8387; fun8337++;
+char *fun07() {
+	int fun17 = 0, fun27 = 0;
+	for (int fun37 = 0; fun47 < fun16 - 1; fun37++) {
+		fun17 = 0;
+		while (fun46[fun37][fun17] || fun46[fun16][fun17]) {
+			fun27 = fun37; if (fun46[fun37][fun17] != fun46[fun16][fun17]) goto fun57; fun17++;
 		}
-		fun8326--;
-		return fun8366[fun8347];
-	fun8397:;
-}
-return fun8366[fun8326];
-}
-
-void fun8308(char fun8334[100]) {
-	int fun8318 = 0;
-	while (fun8334[fun8328]) {
-		fun8356[fun8326][fun8338] = fun8334[fun8318];
-		fun8366[fun8326][fun8318] = fun8334[fun8318];
-		fun8318++;
+		fun16--;
+		return fun56[fun27];
+	fun67:;
 	}
-	fun8356[fun8326][fun8318] = '\0';
-	fun8366[fun8326][fun8318] = '\0';
-	fun8326++;
+	return fun56[fun16];
 }
 
-void fun8348() {
-	fun8308("int");
-	fun8308("char");
-	fun8308("long");
-	fun8308("short");
-	fun8308("for");
-	fun8308("while");
-	fun8308("do");
-	fun8308("void");
-	fun8308("printf");
-	fun8308("scanf");
-	fun8308("getch");
-	fun8308("getchar");
-	fun8308("return");
-	fun8308("fgetc");
-	fun8308("fputc");
-	fun8308("fgets");
-	fun8308("fputs");
-	fun8308("FILE");
-	fun8308("scanf");
-	fun8308("else");
-	fun8308("switch");
-	fun8308("case");
-	fun8308("elseif");
-	fun8308("continue");
-	fun8308("break");
-	fun8308("goto");
-	fun8308("if");
-	fun8308("fopen");
-	fun8308("fclose");
-	fun8308("strlen");
-	fun8308("strcpy");
-	fun8308("strncpy");
-	fun8308("strcmp");
-	fun8308("strncmp");
-	fun8308("strcat");
-	fun8308("strncat");
-	fun8308("feof");
-	fun8308("EOF");
+void fun77(char fun44[100]) {
+	int fun87 = 0;
+	while (fun44[fun97]) {
+		fun46[fun16][fun87] = fun44[fun87];
+		fun56[fun16][fun87] = fun44[fun87];
+		fun87++;
+	}
+	fun46[fun16][fun87] = '\0';
+	fun56[fun16][fun87] = '\0';
+	fun16++;
 }
 
-int fun8358()
+void fun08() {
+	fun77("int");
+	fun77("char");
+	fun77("long");
+	fun77("short");
+	fun77("for");
+	fun77("while");
+	fun77("do");
+	fun77("void");
+	fun77("printf");
+	fun77("scanf");
+	fun77("getch");
+	fun77("getchar");
+	fun77("return");
+	fun77("fgetc");
+	fun77("fputc");
+	fun77("fgets");
+	fun77("fputs");
+	fun77("FILE");
+	fun77("scanf");
+	fun77("else");
+	fun77("switch");
+	fun77("case");
+	fun77("elseif");
+	fun77("continue");
+	fun77("break");
+	fun77("goto");
+	fun77("if");
+	fun77("fopen");
+	fun77("fclose");;
+	fun77("strlen");
+	fun77("strcpy");
+	fun77("strncpy");
+	fun77("strcmp");
+	fun77("strncmp");
+	fun77("strcat");
+	fun77("strncat");
+	fun77("feof");
+	fun77("EOF");
+}
+
+int fun18()
 {
-	int fun8368 = 0;
-	for (int fun8378 = 0; fun8388 < 10000;fun8398++) {
-		int fun8309 = 3;
-		fun8366[fun8378][0] = 'f'; fun8366[fun8378][1] = 'u'; fun8366[fun8378][2] = 'n'; int fun8319 = fun8378; while (fun8329) {
-			fun8366[fun8378][fun8309++] = (fun8339 % 10)+48; fun8319 /= 10;
+	int fun28 = 0;
+	for (int fun38 = 0; fun48 < 10000;fun38++) {
+		int fun58 = 3;
+		fun56[fun38][0] = 'f'; fun56[fun38][1] = 'u'; fun56[fun38][2] = 'n'; int fun68 = fun38; while (fun78) {
+			fun56[fun38][fun58++] = (fun68 % 10) + 48; fun68 /= 10;
 		}
-		fun8366[fun8378][fun8309] = '\0';
+		fun56[fun38][fun58] = '\0';
 	}
-	fun8348();
+	fun08();
 
-	char fun8305[100], fun8349[100] = "bufferOBFSfile.mixno", fun8314 = 'n';
+	char fun15[100], fun88[100] = "bufferOBFSfile.mixno", fun24 = 'n';
 	printf("Input file name:");
-	scanf("%s", &fun8305);
+	scanf("%s", &fun15);
 
-	fun8394(fun8305, fun8359);
+	fun05(fun15, fun98);
 
-	FILE *fun8325 = fopen(fun8369, "r");
+	FILE *fun35 = fopen(fun88, "r");
 	
-	FILE *fun8335 = fopen("Output.txt", "w");
+	FILE *fun45 = fopen("Output.c", "w");
 
-	char fun8379 = 'n';
-	while (!feof(fun8325)) {
-	fun83551:
-		fun8389 = fun8314;
-		fun8314 = fgetc(fun8325);
-		if (feof(fun8325)) { break; }
-
+	char fun09 = 'n';
+	while (!feof(fun35)) {
+	fun551:
+		fun19 = fun24;
+		fun24 = fgetc(fun35);
+		if (feof(fun35)) { break; }
 
 		
-		if (fun8314 == '\"') {
-			fputc('\"', fun8335);
-			fun8383 = 0;
-			fun8393 = 0;
-			fun8324 = 'n';
-			fun8314 = '#';
-			while ((fun8314 != '\"') || (fun8393 % 2 != 0)) {
-			fun83751:fun8324 = fun8314;
-				fun8314 = fgetc(fun8325);
-				if (feof(fun8325)) { goto fun83851; }
-				if (((fun8314 == '\n') && (fun8324 == '\\') && (fun8393 % 2 == 1))) { fputc('\n', fun8335); fun8393 = 0; goto fun83751; }
-				else { if (fun8314 == '\n') { fputc('\n', fun8335); goto fun83551; } }
-				if (fun8314 == '\\') {
-					if (fun8324 == '\"') { fun8393 = 1; }
-					else { fun8393++; }
+		if (fun24 == '\"') {
+			fputc('\"', fun45);
+			fun93 = 0;
+			fun04 = 0;
+			fun34 = 'n';
+			fun24 = '#';
+			while ((fun24 != '\"') || (fun04 % 2 != 0)) {
+			fun751:fun34 = fun24;
+				fun24 = fgetc(fun35);
+				if (feof(fun35)) { goto fun851; }
+				if (((fun24 == '\n') && (fun34 == '\\') && (fun04 % 2 == 1))) { fputc('\n', fun45); fun04 = 0; goto fun751; }
+				else { if (fun24 == '\n') { fputc('\n', fun45); goto fun551; } }
+				if (fun24 == '\\') {
+					if (fun34 == '\"') { fun04 = 1; }
+					else { fun04++; }
 				}
-				else { if (fun8314 != '\"') { fun8393 = 0; } }
-				fputc(fun8314, fun8335);
+				else { if (fun24 != '\"') { fun04 = 0; } }
+				fputc(fun24, fun45);
 			}
 			
-			goto fun83551;
+			goto fun551;
 		}
 		else {
 			
-			if (fun8314 == '\'') {
+			
+			if (fun24 == '\'') {
 				fputc('\'', fout);
-				fun8383 = 0;
-				fun8393 = 0;
-				fun8324 = 'n';
-				fun8314 = '#';
-				while ((fun8314 != '\'') || (slkol % 2 != 0)) {
-					fun8324 = fun8314;
-					fun8314 = fgetc(fun8325);
-					if (feof(fun8325)) { goto fun83851; }
-					if (fun8314 == '\n') { fputc('\n', fun8335); goto fun83551; }
-					if (fun8314 == '\\') {
-						if (fun8324 == '\'') { slkol = 1; }
-						else { fun8393++; }
+				fun93 = 0;
+				fun04 = 0;
+				fun34 = 'n';
+				fun24 = '#';
+				while ((fun24 != '\'') || (slkol % 2 != 0)) {
+					fun34 = fun24;
+					fun24 = fgetc(fun35);
+					if (feof(fun35)) { goto fun851; }
+					if (fun24 == '\n') { fputc('\n', fun45); goto fun551; }
+					if (fun24 == '\\') {
+						if (fun34 == '\'') { slkol = 1; }
+						else { fun04++; }
 					}
-					else { if (fun8314 != '\'') { slkol = 0; } }
-					fputc(fun8314, fun8335);
+					else { if (fun24 != '\'') { slkol = 0; } }
+					fputc(fun24, fun45);
 				}
-				goto fun83551;
+				goto fun551;
 			}
 			else {
 				
 
-
-				if (fun8368 == 0) {
-					if (fun8314 == '#') { char fun8399[1000]; fgets(fun83001, 1000, fun8325); fputc('#', fun8335); fputs(fun83101, fun8335); continue; }
+				if (fun28 == 0) {
+					if (fun24 == '#') { char fun29[1000]; fgets(fun39, 1000, fun35); fputc('#', fun45); fputs(fun29, fun45); continue; }
 				}
-				if (fun8314 == '{') fun8368 = 1;
-				if ((fun8379 == ' ') && (fun8314 == ' ')) { continue; }
-				if (fun8376(fun8314)) { fun8316 = 1; fun8356[fun8326][fun8336++] = fun8314; }
+				if (fun24 == '{') fun28 = 1;
+				if ((fun09 == ' ') && (fun24 == ' ')) { continue; }
+				if (fun66(fun24)) { if (fun06 == 0) fun16++; fun06 = 1; fun46[fun16][fun26++] = fun24; }
 				else {
-					if (fun8316 == 1) {
-						fun8356[fun8326][fun8336] = '\0'; fun8336 = 0; 
-						fputs(fun8327(), fun8335);
-						fun8326++;
+					if (fun06 == 1) {
+						fun46[fun16][fun26] = '\0'; fun26 = 0; if (fun16 == 8) {
+
+						}
+						fputs(fun07(), fun45);
 					}
-					fun8316 = 0;
-					fputc(fun8314, fun8335);
+					fun06 = 0;
+					fputc(fun24, fun45);
 				}
 			}
 		}
 	}
-		fun83851:
-			fclose(fun8325);
-			fclose(fun8335);
+fun851:;
 
 	return 0;
 }
--- obfus/obfus/obfus.cpp
+++ obfus/obfus/obfus.cpp
@@ -2,13 +2,14 @@
 #include <stdio.h> 
 #include <string.h> 
 
+int i = 0, slkol = 0; //slkol - количество слешей, идущих подр€д
+char cArray[10000], c = 'n', lc = 'n', s[10000], cc = 'n', ccc = 'n';
+int len = strlen(cArray);
+int kav1 = 0, kav2 = 0; //  авычки. 1 - '_', 2 - "_"; 
+
 void delComments(char inFile[], char outFile[]) {
 	FILE *fin = fopen(inFile, "r");
 	FILE *fout = fopen(outFile, "w");
-	int i = 0, slkol = 0; //slkol - количество слешей, идущих подр€д
-	char cArray[10000], c = 'n', lc = 'n', s[10000], cc = 'n', ccc = 'n';
-	int len = strlen(cArray);
-	int kav1 = 0, kav2 = 0; //  авычки. 1 - '_', 2 - "_"; 
 
 	for (i = 0; i<10000; i++) { s[i] = cArray[i] = '\0'; }
 
@@ -171,15 +172,15 @@ void launch() {
 	take("if");
 	take("fopen");
 	take("fclose");;
-	fun08("strlen");
-	fun08("strcpy");
-	fun08("strncpy");
-	fun08("strcmp");
-	fun08("strncmp");
-	fun08("strcat");
-	fun08("strncat");
-	fun08("feof");
-	fun08("EOF");
+	take("strlen");
+	take("strcpy");
+	take("strncpy");
+	take("strcmp");
+	take("strncmp");
+	take("strcat");
+	take("strncat");
+	take("feof");
+	take("EOF");
 }
 
 int main()
@@ -202,30 +203,84 @@ int main()
 
 	FILE *fin = fopen(bufFile, "r");
 	//FILE *fout = fopen(inFile, "w");
-	FILE *fout = fopen("Output.txt", "w");
+	FILE *fout = fopen("Output.c", "w");
 
 	char last = 'n';
 	while (!feof(fin)) {
+	xyz1:
 		last = c;
 		c = fgetc(fin);
 		if (feof(fin)) { break; }
-		if (def == 0) {
-			if (c == '#') { char ss[1000]; fgets(ss, 1000, fin); fputc('#', fout); fputs(ss, fout); continue; }
-		}
-		if (c == '{') def = 1;
-		if ((last == ' ') && (c == ' ')) { continue; }
-		if (isWord(c)) { if (word == 0) countOfWords++; word = 1; words[countOfWords][whereWord++] = c; }
-		else {
-			if (word == 1) {
-				words[countOfWords][whereWord] = '\0';  whereWord = 0; if (countOfWords == 8) {
 
+		//кавычки типа "_" 
+		if (c == '\"') {
+			fputc('\"', fout);
+			i = 0;
+			slkol = 0;
+			lc = 'n';
+			c = '#';
+			while ((c != '\"') || (slkol % 2 != 0)) {
+			repa1:lc = c;
+				c = fgetc(fin);
+				if (feof(fin)) { goto tuda1; }
+				if (((c == '\n') && (lc == '\\') && (slkol % 2 == 1))) { fputc('\n', fout); slkol = 0; goto repa1; }
+				else { if (c == '\n') { fputc('\n', fout); goto xyz1; } }
+				if (c == '\\') {
+					if (lc == '\"') { slkol = 1; }
+					else { slkol++; }
 				}
-				fputs(changeWord(), fout);
+				else { if (c != '\"') { slkol = 0; } }
+				fputc(c, fout);
 			}
-			word = 0;
-			fputc(c, fout);
+			//fputc('#', fout); 
+			goto xyz1;
+		}
+		else {
+			//TODO ”йти в кулинарный 
+			//кавычки типа '_' 
+			if (c == '\'') {
+				fputc('\'', fout);
+				i = 0;
+				slkol = 0;
+				lc = 'n';
+				c = '#';
+				while ((c != '\'') || (slkol % 2 != 0)) {
+					lc = c;
+					c = fgetc(fin);
+					if (feof(fin)) { goto tuda1; }
+					if (c == '\n') { fputc('\n', fout); goto xyz1; }
+					if (c == '\\') {
+						if (lc == '\'') { slkol = 1; }
+						else { slkol++; }
+					}
+					else { if (c != '\'') { slkol = 0; } }
+					fputc(c, fout);
+				}
+				goto xyz1;
+			}
+			else {
+				//.
+
+				if (def == 0) {
+					if (c == '#') { char ss[1000]; fgets(ss, 1000, fin); fputc('#', fout); fputs(ss, fout); continue; }
+				}
+				if (c == '{') def = 1;
+				if ((last == ' ') && (c == ' ')) { continue; }
+				if (isWord(c)) { if (word == 0) countOfWords++; word = 1; words[countOfWords][whereWord++] = c; }
+				else {
+					if (word == 1) {
+						words[countOfWords][whereWord] = '\0';  whereWord = 0; if (countOfWords == 8) {
+
+						}
+						fputs(changeWord(), fout);
+					}
+					word = 0;
+					fputc(c, fout);
+				}
+			}
 		}
 	}
+tuda1:;
 
 	return 0;
 }
