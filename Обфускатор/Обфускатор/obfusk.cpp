57ed98640d9cca28db08d899c4adb9b23813b01c
Arkagami <mixno20@gmail.com>
2017-12-23 17:11:37 +0300

    12

--- Обфускатор/Обфускатор/obfusk.cpp
+++ Обфускатор/Обфускатор/obfusk.cpp
@@ -7,106 +7,6 @@ char cArray[10000], c = 'n', lc = 'n', s[10000], cc = 'n', ccc = 'n';
 int len = strlen(cArray);
 int kav1 = 0, kav2 = 0; // Кавычки. 1 - '_', 2 - "_"; 
 
-/*void delComments(char inFile[], char outFile[]) {
-  FILE *fin = fopen(inFile, "r");
-  FILE *fout = fopen(outFile, "w");
-
-  for (i = 0; i<10000; i++) { s[i] = cArray[i] = '\0'; }
-
-  while (!feof(fin)) {
-  xyz:
-    c = fgetc(fin);
-    if (feof(fin)) { break; }
-  eto:
-    //кавычки типа "_" 
-    if (c == '\"') {
-      fputc('\"', fout);
-      i = 0;
-      slkol = 0;
-      lc = 'n';
-      c = '#';
-      while ((c != '\"') || (slkol % 2 != 0)) {
-      repa:lc = c;
-        c = fgetc(fin);
-        if (feof(fin)) { goto tuda; }
-        if (((c == '\n') && (lc == '\\') && (slkol % 2 == 1))) { fputc('\n', fout); slkol = 0; goto repa; }
-        else { if (c == '\n') { fputc('\n', fout); goto xyz; } }
-        if (c == '\\') {
-          if (lc == '\"') { slkol = 1; }
-          else { slkol++; }
-        }
-        else { if (c != '\"') { slkol = 0; } }
-        fputc(c, fout);
-      }
-      //fputc('#', fout); 
-      goto xyz;
-    }
-    else {
-      //TODO Уйти в кулинарный 
-      //кавычки типа '_' 
-      if (c == '\'') {
-        fputc('\'', fout);
-        i = 0;
-        slkol = 0;
-        lc = 'n';
-        c = '#';
-        while ((c != '\'') || (slkol % 2 != 0)) {
-          lc = c;
-          c = fgetc(fin);
-          if (feof(fin)) { goto tuda; }
-          if (c == '\n') { fputc('\n', fout); goto xyz; }
-          if (c == '\\') {
-            if (lc == '\'') { slkol = 1; }
-            else { slkol++; }
-          }
-          else { if (c != '\'') { slkol = 0; } }
-          fputc(c, fout);
-        }
-        goto xyz;
-      }
-      else {
-        //. 
-
-        //Комментарии 
-        if (c == '/') {
-          c = fgetc(fin); if (feof(fin)) { fputc('/', fout); goto tuda; }
-          if (c == '/') {
-            fputc('\n', fout); while (1) {
-              lc = c;
-              c = fgetc(fin);
-              if (feof(fin)) { goto tuda; }
-              if (c == '\n') {
-                if (lc == '\\') {
-                  continue;
-                }
-                else { break; }
-              }
-            }
-            goto xyz;
-          }
-          else {
-            if (c == '*') {
-              cc = 'i'; ccc = 'i'; while ((cc != '*') || (ccc != '/')) { cc = ccc; ccc = fgetc(fin); if (feof(fin)) { goto tuda; } } goto xyz;
-            }
-            else {
-              fputc('/', fout);  goto eto;
-            }
-          }
-          goto xyz;
-        }
-        else { fputc(c, fout); }
-      }
-    }
-
-    //----------------------Комментарии 
-
-  }
-tuda:
-  fclose(fin);
-  fclose(fout);
-}*/
-
-
 int word = 0, countOfWords = 0, whereWord = 0;
 char words[10000][100], chang[10000][100];
 //Проверяет символ на принадлежность к английскому алфавиту 
@@ -119,7 +19,7 @@ int isWord(char qwertyuiop) {
 
 char *changeWord() {
   int e = 0, save = 0;
-  for (int tyoh = 0; tyoh < countOfWords - 1; tyoh++) {
+  for (int tyoh = 0; tyoh < countOfWords; tyoh++) {
     e = 0;
     while (words[tyoh][e] || words[countOfWords][e]) {
       save = tyoh; if (words[tyoh][e] != words[countOfWords][e]) goto met; e++;
@@ -159,6 +59,8 @@ void launch() {
   take("return");
   take("fgetc");
   take("fputc");
+  take("getc");
+  take("putc");
   take("fgets");
   take("fputs");
   take("FILE");
@@ -182,6 +84,15 @@ void launch() {
   take("strncat");
   take("feof");
   take("EOF");
+  take("unsigned");
+  take("isdigit");
+  take("main");
+  take("double");
+  take("sin");
+  take("cos");
+  take("pow");
+  take("powl");
+  take("pi");
 }
 
 int main()
@@ -196,14 +107,12 @@ int main()
   }
   launch();
 
-  char inFile[100] = "in2.c", bufFile[100] = "bufferOBFSfile.mixno", c = 'n';
+  char inFile[100] = "in2.c", c = 'n';
   //printf("Input file name:");
   //scanf("%s", &inFile);
 
-  //delComments(inFile, bufFile);
 
-  FILE *fin = fopen(bufFile, "r");
-  //FILE *fout = fopen(inFile, "w");
+  FILE *fin = fopen(inFile, "r");
   FILE *fout = fopen("Output.c", "w");
 
   char last = 'n';
@@ -285,7 +194,8 @@ int main()
               cc = 'i'; ccc = 'i'; while ((cc != '*') || (ccc != '/')) { cc = ccc; ccc = fgetc(fin); if (feof(fin)) { goto tuda1; } } goto xyz1;
             }
             else {
-              fputc('/', fout);  goto eto;
+              fputc('/', fout);  
+              goto eto;
             }
           }
           goto xyz1;
